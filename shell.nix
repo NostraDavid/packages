0d9bd6c749ca89b8a101ceabddddbdf7a69a90fe
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2024-10-08 23:48:35 +0200

    update shell.nix?

--- shell.nix
+++ shell.nix
@@ -2,26 +2,16 @@
 
 pkgs.mkShell {
   buildInputs = [
+    pkgs.cabal-install
     pkgs.ghc
     pkgs.stack
-    pkgs.cabal-install
     pkgs.zlib
-    pkgs.zlib.dev
-    pkgs.libpng
-    pkgs.libjpeg
-    pkgs.glibc
-    pkgs.glibc.dev
   ];
 
   shellHook = ''
     export LD_LIBRARY_PATH=${
       pkgs.lib.makeLibraryPath [
         pkgs.zlib
-        pkgs.zlib.dev
-        pkgs.libpng
-        pkgs.libjpeg
-        pkgs.glibc
-        pkgs.glibc.dev
       ]
     }
   '';
